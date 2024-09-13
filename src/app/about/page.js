3367423136034580ca08d4cb7c2f0adbda2251da
carlos <twincarlos98@gmail.com>
2024-09-13 02:12:03 -0400

    added core values

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,6 +1,7 @@
 import "./About.css";
 import Image from "next/image";
 import aboutImg from "@/app/assets/about-img.png";
+import HomeComponent6 from "../components/HomeComponent6/HomeComponent6";
 
 export default function About() {
     return (
@@ -11,6 +12,7 @@ export default function About() {
                 <p>Following a life-changing journey to the jungles of Costa Rica’s Central Pacific Coast. While volunteering at a remote animal sanctuary, Gabriel witnessed the profound impact a small facility could have on conservation and the lives of rainforest animals. Captivated by the parrots, monkeys, sloths, and other wildlife, he returned to the US with a burning passion to aid these incredible creatures and make a significant contribution to conservation efforts.</p>
                 <p>Fueled by inspiration, Gabriel acquired land and began constructing an animal preserve. He rescued parrots in need and diligently researched federal zoo regulations. Through perseverance and dedication, he obtained his USDA Zoo Exhibitors license, enabling him to host field trips and visit schools nationwide to educate students about wildlife and the challenges they face in the wild.</p>
             </div>
+            <HomeComponent6 />
         </main>
     );
 };
