531289baa29ca8de8b59b9a20d9d3c66361cd58e
carlos <twincarlos98@gmail.com>
2024-09-16 20:06:51 -0400

    done

--- src/app/components/HomeComponent2/HomeComponent2.js
+++ src/app/components/HomeComponent2/HomeComponent2.js
@@ -7,7 +7,7 @@ import homeComponentImg2 from "@/app/assets/home-component-2-img-2.png";
 export default function HomeComponent2() {
     return (
         <section className="home-component home-component-2">
-            <div className="mobile">
+            {/* <div className="mobile">
                 <div className="container">
                     <p className="title">Animal Adventure Squad was founded in 2012 by Gabriel Ligon,</p>
                     <div className="paragraph">
@@ -19,7 +19,30 @@ export default function HomeComponent2() {
                 <div className="Large container">
                     <Image src={homeComponentImg} />
                 </div>
+            </div> */}
+            <div className="mobile">
+                <div className="container">
+                    <p className="title">Wild Encounters, Real Learning: Bringing the Zoo to You!</p>
+                    <div className="paragraph">
+                        <p className="text">At <strong>Animal Adventure Squad</strong>, we believe learning about nature should be exciting, interactive, and unforgettable! Our traveling team brings a variety of live animals straight to your school, giving students the chance to meet incredible creatures like tortoises, parrots, kangaroos, and even an axolotl! Through hands-on animal encounters, students will gain a deeper understanding of conservation, biology, and the natural world sparking curiosity and a love for wildlife. Get ready to watch their eyes light up as they learn about animal adaptations, ecosystems, and more in a fun and engaging way!</p>
+                    </div>
+                </div>
+                <div className="container">
+                    <Image src={homeComponentImg1} />
+                </div>
             </div>
+            <div className="mobile">
+                <div className="container">
+                    <p className="title">Aligned with National Science Standards</p>
+                    <div className="paragraph">
+                        <p className="text">Our programs are designed to make learning both meaningful and memorable! We tailor our animal shows to fit the specific science curriculum for each grade level. Whether it’s exploring animal life cycles with 2nd and 3rd graders or discussing ecosystems and adaptations with 4th and 5th graders, our educators bring concepts to life through real animal ambassadors. We even help improve science testing scores by reinforcing key topics with exciting, real-world examples—making learning not just an experience but an adventure!</p>
+                    </div>
+                </div>
+                <div className="container">
+                    <Image src={homeComponentImg2} />
+                </div>
+            </div>
+
             <div className="desktop">
                 <div className="Large container">
                     <Image src={homeComponentImg1} />
