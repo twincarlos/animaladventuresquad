17ecf3016a4c8b87551127f21ea8700cc8029980
carlos <twincarlos98@gmail.com>
2024-09-16 21:34:56 -0400

    more stuff

--- src/app/components/HomeComponent1/HomeComponent1.js
+++ src/app/components/HomeComponent1/HomeComponent1.js
@@ -1,16 +1,19 @@
 import "./HomeComponent1.css";
 import Image from "next/image";
 import homeComponentImg from "@/app/assets/home-component-1-img.png";
+import homeComponentImgMobile from "@/app/assets/home-component-1-img-mobile.png";
 import Link from "next/link";
 
 export default function HomeComponent1() {
     return (
         <section className="home-component home-component-1">
-            <Image src={homeComponentImg} />
+            <Image className="desktop" src={homeComponentImg} />
+            <Image className="mobile" src={homeComponentImgMobile} />
             <div className="book-a-visit-container">
                 <Link href={"/contact-us"}>BOOK A VISIT</Link>
                 <h2>Free for Your School!</h2>
-                <p>Want to bring our exciting animal programs to your school for FREE? It’s easy! We offer complimentary educational sessions to schools with 300 or more students. All we ask in return is that students have the opportunity to take a photo with one of our animal ambassadors to help fund our program.</p>
+                <p className="desktop">Want to bring our exciting animal programs to your school for FREE? It’s easy! We offer complimentary educational sessions to schools with 300 or more students. All we ask in return is that students have the opportunity to take a photo with one of our animal ambassadors to help fund our program.</p>
+                <p className="mobile">Want to bring our exciting animal programs to your school for FREE?…..</p>
             </div>
         </section>
     );
