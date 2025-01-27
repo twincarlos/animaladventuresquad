457f1733d614f7d41aad0283e477cdc2641878f3
carlos rodriguez <88858893+twincarlos@users.noreply.github.com>
2025-01-27 00:34:32 -0500

    Create mailchimp.js

---
+++ src/app/actions/mailchimp.js
@@ -0,0 +1,29 @@
+"use server";
+
+export async function createTournament(initialState, formData) {
+  const firstName = formData.get("first-name");
+  const lastName = formData.get("last-name");
+  const subject = formData.get("subject");
+  const message = formData.get("message");
+  const email = formData.get("email");  
+
+  const data = {
+    email_address: email,
+    status: "subscribed",
+    merge_fields: {
+      FNAME: firstName,
+      LNAME: lastName,
+      SUBJECT: subject,
+      MESSAGE: message
+    }
+  };
+  
+  const response = await fetch(`https://us20.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}/members/`, {
+    method: "POST",
+    headers: {
+      "Authorization": `apiKey ${process.env.MAILCHIMP_API_KEY}`,
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify(data)
+  });
+};
