e50929e98536eceef432a10cfe0d5b3692f96cd5
carlos rodriguez <88858893+twincarlos@users.noreply.github.com>
2025-01-27 00:56:25 -0500

    Update mailchimp.js

--- src/app/actions/mailchimp.js
+++ src/app/actions/mailchimp.js
@@ -17,10 +17,8 @@ export async function mailchimp (formData) {
       MESSAGE: message
     }
   };
-
-  console.log(data);
   
-  const response = await fetch(`https://us20.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}/members/`, {
+  const response = await fetch(`https://us7.api.mailchimp.com/3.0/lists/${process.env.MAILCHIMP_LIST_ID}/members/`, {
     method: "POST",
     headers: {
       "Authorization": `apiKey ${process.env.MAILCHIMP_API_KEY}`,
