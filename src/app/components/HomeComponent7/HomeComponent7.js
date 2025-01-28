dbb33548ae9165cb015bcc4e7b6f35481d3dbe74
carlos rodriguez <88858893+twincarlos@users.noreply.github.com>
2025-01-28 01:02:25 -0500

    Update HomeComponent7.js

--- src/app/components/HomeComponent7/HomeComponent7.js
+++ src/app/components/HomeComponent7/HomeComponent7.js
@@ -1,6 +1,8 @@
+"use client";
 import "./HomeComponent7.css";
 import Image from "next/image";
 import logo from "@/app/assets/header-logo.png";
+import { mailchimp } from "@/app/actions/mailchimp.js";
 
 export default function HomeComponent7() {
     return (
@@ -8,8 +10,10 @@ export default function HomeComponent7() {
             <div className="widget">
                 <Image src={logo} />
                 <p className="title">Keep in touch With Us!</p>
-                <input type="text" placeholder="E-mail" />
-                <button>Subscribe</button>
+                <form action={mailchimp}>
+                    <input type="text" name="email" placeholder="E-mail" />
+                    <button type="submit" onClick={() => alert("Thanks for subscribing!")}>Subscribe</button>
+                </form>
                 <p className="widget-footer">We will only send you an email once per Month at most!</p>
             </div>
         </section>
